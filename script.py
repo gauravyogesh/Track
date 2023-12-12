b050f6fee00b09e68379a9410e95537293453f0a
gauravyogesh <gyogeshwar0@gmail.com>
2023-12-12 15:55:23 +0530

    'www'

--- script.py
+++ script.py
@@ -26,17 +26,19 @@ total_len = len(df)
 
 for i in range(1,total_len):
     print(i)
+    print(df)
+    print("-"* 100)
 
-    time.sleep(30)
+    time.sleep(120)
 
     df['Status'].iloc[i] = True
     df['Status'].iloc[i-1] = False
 
     df.to_csv('saved.csv', index= False)
 
-    time.sleep(30)
+    time.sleep(5)
 
     subprocess.run("git add .", shell=True, stdout=subprocess.PIPE, text=True)
-    subprocess.run(f"git commit -m 'www'", shell=True, stdout=subprocess.PIPE, text=True)
+    subprocess.run(f"git commit -m '{datetime.now()}'", shell=True, stdout=subprocess.PIPE, text=True)
     subprocess.run(f"git push origin main", shell=True, stdout=subprocess.PIPE, text=True)
 
